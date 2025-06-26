69eda4b89ff8b451c278e15e602d8d223f655bd3
lilzy218 <lily.schroder@gmail.com>
2025-06-26 16:06:22 +0100

    added search functionality using API for humidity and windspeed

--- src/app.js
+++ src/app.js
@@ -2,8 +2,17 @@ function refreshWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+  console.log(response.data);
+
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML = response.data.condition.description;
   temperatureElement.innerHTML = Math.round(temperature);
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
 }
 function searchCity(city) {
   let apiKey = "4a90co310af353688etc1f6b420b6eed";
